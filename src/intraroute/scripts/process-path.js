c2f8a4af40b5232de4489cfac8be9ee38e1183ac
KittyCat11231 <63092830+KittyCat11231@users.noreply.github.com>
2024-05-09 17:01:51 -0400

    Update process-path.js

--- src/intraroute/scripts/process-path.js
+++ src/intraroute/scripts/process-path.js
@@ -1,1 +1,11 @@
 import { finalPath } from './pathfinding.js';
+
+import airRoutes from '../data/ui/routes/air.json';
+import airStops from '../data/ui/stops/air.json';
+
+import bahnRoutes from '../data/ui/routes/bahn.json';
+import bahnStops from '../data/ui/stops/bahn.json';
+
+import busRoutes from '../data/ui/routes/bus.json';
+import busStops from '../data/ui/stops/bus.json';
+
