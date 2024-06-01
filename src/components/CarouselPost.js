3cfcf964251d0686e1e32e4cba4f7a72b7944b57
KittyCat11231 <kittycat61500@gmail.com>
2024-06-01 17:26:40 -0400

    committing just so i can revert

--- src/components/CarouselPost.js
+++ src/components/CarouselPost.js
@@ -1,52 +1,25 @@
 import React from 'react';
-import { useState } from 'react';
 import styles from './CarouselPost.module.css';
 
-function CarouselPost(props) {
-    const [buttonTextColor, setButtonTextColor] = useState('white');
+import CarouselPostDesktop from './CarouselPostDesktop';
 
+function CarouselPost(props) {
     return (
-        <div className={styles.container}>
-            <div className={styles.imgBox} style={{backgroundColor: props.imgBoxColor}}>
-                <img
-                    className={styles.img}
-                    src={props.imgSrc}
-                    alt={props.imgAlt}
-                    style={{
-                        width: props.imgWidth,
-                        maxWidth: props.imgMaxWidth,
-                        height: props.imgHeight,
-                        maxHeight: props.imgMaxHeight,
-                        transform: props.imgTransform,
-                    }}
-                />
-            </div>
-            <div className={styles.contentBox} style={{backgroundColor: props.color}}>
-                <div className={styles.textContainer}>
-                    <h1 className={`${styles.header} ${styles.text}`}>
-                        {props.headerText}
-                    </h1>
-                    <h2 className={`${styles.descriptor} ${styles.text}`}>
-                        {props.descriptorText}
-                    </h2>
-                    <div
-                        className={styles.button}
-                        onMouseEnter={() => setButtonTextColor(props.color)}
-                        onMouseLeave={() => setButtonTextColor('white')}
-                    >
-                        <a
-                            href={props.link}
-                            className={`${styles.buttonText} ${styles.text}`}
-                            style={{
-                                color: buttonTextColor,
-                            }}
-                        >
-                            {props.buttonText}
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <CarouselPostDesktop
+            color={props.color}
+            imgBoxColor={props.imgBoxColor}
+            imgSrc={props.imgSrc}
+            imgAlt={props.imgAlt}
+            imgWidth={props.imgWidthDesktop}
+            imgMaxWidth={props.imgMaxWidthDesktop}
+            imgHeight={props.imgHeightDesktop}
+            imgMaxHeight={props.imgMaxHeightDesktop}
+            imgTransform={props.imgTransformDesktop}
+            headerText={props.headerText}
+            descriptorText={props.descriptorText}
+            buttonText={props.buttonText}
+            link={props.link}
+        />
     )
 }
 
