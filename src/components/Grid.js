a34b0349889684b9528f3faaaf80e6b8c60fa3fd
KittyCat11231 <kittycat61500@gmail.com>
2024-06-02 15:42:51 -0400

    home page elements

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,8 +5,33 @@ import GridPost from './GridPost';
 
 function Grid(props) {
     return (
-        <div className={styles.container}>
-            <GridPost />
+        <div className={styles.outerContainer}>
+            <div className={styles.headerContainer}>
+                <h1 className={styles.header}>Our Brands</h1>
+            </div>
+            <div className={styles.container}>
+                <div className={styles.gridPost}>
+                    <GridPost />
+                </div>
+                <div className={styles.gridPost}>
+                    <GridPost />
+                </div>
+                <div className={styles.gridPost}>
+                    <GridPost />
+                </div>
+                <div className={styles.gridPost}>
+                    <GridPost />
+                </div>
+                <div className={styles.gridPost}>
+                    <GridPost />
+                </div>
+                <div className={styles.gridPost}>
+                    <GridPost />
+                </div>
+                <div className={styles.gridPost}>
+                    <GridPost />
+                </div>
+            </div>
         </div>
     )
 }
