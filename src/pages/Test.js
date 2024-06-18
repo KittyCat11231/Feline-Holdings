a7abbde43db16980bcc8003754b37cf5deaf1754
KittyCat11231 <kittycat61500@gmail.com>
2024-06-17 23:32:03 -0400

    test page

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,67 +1,12 @@
 import React from 'react';
-import { useState, useRef } from 'react';
 import { Helmet } from 'react-helmet';
 import styles from './Test.module.css';
 
-import airStops from '../intraroute/data/pathfinding/air.json';
-import bahnStops from '../intraroute/data/pathfinding/bahn.json';
-import busStops from '../intraroute/data/pathfinding/bus.json';
-import omegaStops from '../intraroute/data/pathfinding/omega.json';
-import railStops from '../intraroute/data/pathfinding/rail.json';
-import railLumevaStops from '../intraroute/data/pathfinding/railLumeva.json';
-import railScarStops from '../intraroute/data/pathfinding/railScar.json';
-import sailStops from '../intraroute/data/pathfinding/sail.json';
-
 function Test() {
-    let validIds = [];
     
-    function addValidIds(modeStops) {
-        modeStops.forEach(stop => {
-            validIds.push(stop.id);
-        })
-    }
-
-    addValidIds(airStops);
-    addValidIds(bahnStops);
-    addValidIds(busStops);
-    addValidIds(omegaStops);
-    addValidIds(railStops);
-    addValidIds(railLumevaStops);
-    addValidIds(railScarStops);
-    addValidIds(sailStops);
-
-    const [inputValue, setInputValue] = useState('');
-
-    const baseUrl = window.location.href;
-    let url = new URL(baseUrl);
-    if (!(validIds.includes(url.searchParams.get('start')))) {
-        let urlSplit = baseUrl.toString().split('?');
-        url = urlSplit[0];
-        url = new URL(url);
-    }
-    url.searchParams.forEach((value, key) => {
-        if (key !== 'start') {
-            url.searchParams.delete(key);
-        }
-    })
-
-    function handleChange(e) {
-        setInputValue(e.target.value);
-    }
-
-    function handleClick() {
-        if (validIds.includes(inputValue)) {
-            url.searchParams.delete('start');
-            url.searchParams.append('start', inputValue);
-            window.location.replace(url);
-        }
-    }
-
-    const [testColor, setTestColor] = useState('red');
-    const testDiv = useRef();
 
     return (
-        <div>
+        <>
             <Helmet>
                 <title>Test Page | Feline Holdings</title>
         
@@ -74,25 +19,8 @@ function Test() {
                 <meta name="twitter:title" content="Test Page | Feline Holdings" />
                 <meta name="twitter:description" content="Feline Holdings development test page" />
             </Helmet>
-            <h1>Development test page</h1>
-            <button onClick={handleClick}>Test</button>
-            <input onChange={e => handleChange(e)} />
-            <p>{url.toString()} text after url</p>
-
-            <div
-                className={styles.testDiv}
-                ref={testDiv}
-                onClick={() => {
-                    if (testColor === 'red') {
-                        setTestColor('lime');
-                    } else if (testColor === 'lime') {
-                        setTestColor('red');
-                    }
-                    testDiv.current.style.setProperty('--background-color', testColor);
-                }}
-            >
-            </div>
-        </div>
+            <h1>Placeholder</h1>
+        </>
     )
 }
 
