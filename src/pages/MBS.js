a2220e89cded17efc8c5165e4220bfb15bec7284
KittyCat11231 <kittycat61500@gmail.com>
2024-06-20 14:33:19 -0400

    added icon and page title for MBS

--- src/pages/MBS.js
+++ src/pages/MBS.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import { Helmet } from 'react-helmet';
 import styles from './MBS.module.css';
 
 import YouTubeEmbed from '../components/YouTubeEmbed';
@@ -33,9 +34,19 @@ function MBS() {
     });
 
     return (
-        <div className={styles.container}>
-            {isLive ? <LiveFrame /> : ''}
-        </div>
+        <>
+            <Helmet>
+                <title>MBS | Feline Holdings</title>
+                <meta property="og:title" content="MBS | Feline Holdings" />
+                <meta name="twitter:title" content="MBS | Feline Holdings" />
+                <meta name="description" content="The MRT's most watched broadcaster." />
+                <link rel="icon" href='https://svgshare.com/i/17Sp.svg' />
+                <link rel="apple-touch-icon" href='https://svgshare.com/i/17Sp.svg' />
+            </Helmet>
+            <div className={styles.container}>
+                {isLive ? <LiveFrame /> : ''}
+            </div>
+        </>
     )
 }
 
