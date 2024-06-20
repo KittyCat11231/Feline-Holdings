f6d0095bc46f7f9c7a33673df3f7c6a1084a0be8
KittyCat11231 <kittycat61500@gmail.com>
2024-06-20 13:04:02 -0400

    testing stuff

--- src/pages/MBS.js
+++ src/pages/MBS.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import styles from './MBS.module.css';
 
+function LiveFrame() {
+    return (
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/live_stream?channel=UCdqFWzZ2sTEM3svKajyk9Lg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    )
+}
+
 function MBS() {
-    let channelid = "UCdqFWzZ2sTEM3svKajyk9Lg";
+    const channelid = "UCdqFWzZ2sTEM3svKajyk9Lg";
+
+    let isLive = false;
 
     function onlive() {
-        console.log('This channel is live!')
+        console.log('This channel is live!');
+        isLive = true;
     }
 
-    fetch('https://www.youtube.com/channel/' + channelid).then(function (response) {
+    fetch('https://feline-holdings-cors.kittycat61500.workers.dev/?https://www.youtube.com/channel/' + channelid)
+    .then(function (response) {
+        console.log(response);
         return response.text();
     }).then(function (html) {
         if (html.includes("hqdefault_live.jpg")) {
             onlive();
         }
     }).catch(function (err) {
-        console.warn('This channel is not live.', err);
+        console.warn('Failed to fetch.', err);
     });
 
     return (
         <div className={styles.container}>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/live_stream?channel=UCdqFWzZ2sTEM3svKajyk9Lg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            {isLive ? <LiveFrame /> : ''}
         </div>
     )
 }
