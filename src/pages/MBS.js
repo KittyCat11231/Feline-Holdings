f18bc945aebd10bd8013f35d3cc5379bf716020e
KittyCat11231 <kittycat61500@gmail.com>
2024-06-24 18:31:09 -0400

    MBS work

--- src/pages/MBS.js
+++ src/pages/MBS.js
@@ -3,8 +3,68 @@ import { Helmet } from 'react-helmet';
 import styles from './MBS.module.css';
 
 import LiveMBS from '../components/LiveMBS';
+import Carousel from '../components/Carousel';
+import CarouselPost from '../components/CarouselPost';
+
+import mbsLogoMain from '../logos/mbs-main.svg';
+import mbsLogoWhite from '../logos/mbs-main-white.svg';
+import mbsLogoBlack from '../logos/mbs-main-black.svg';
+
+import mbsMls from '../assets/mbs-mls.svg';
+import adTeaser from '../assets/mbs-advertising-teaser.svg';
 
 function MBS() {
+
+    let carouselProps = {
+        post1: {
+            color: 'blue',
+            imgBoxColor: 'white',
+            imgSrc: mbsLogoMain,
+            imgAlt: 'MBS',
+            imgWidth: '60%',
+            imgMaxWidth: undefined,
+            imgHeight: undefined,
+            imgMaxHeight: '95%',
+            imgTransform: undefined,
+            headerText: 'The MRT\'s broadcasting system.',
+            descriptorText: 'MBS brings the MRT to you.',
+            renderButton: true,
+            buttonText: 'Watch MBS',
+            link: 'https://www.youtube.com/@mbs_mrt', 
+        },
+        post2: {
+            color: '#1a2859',
+            imgBoxColor: '#e22525',
+            imgSrc: mbsMls,
+            imgAlt: 'MLS on MBS',
+            imgWidth: undefined,
+            imgMaxWidth: '1000px',
+            imgHeight: '100%',
+            imgMaxHeight: undefined,
+            imgTransform: undefined,
+            headerText: 'Watch MLS Season 8 on MBS.',
+            descriptorText: "Major League Spleef is back.",
+            renderButton: true,
+            buttonText: 'Watch Now',
+            link: 'https://felineholdings.com/MBS', 
+        },
+        post3: {
+            color: 'red',
+            imgBoxColor: '#333333',
+            imgSrc: adTeaser,
+            imgAlt: 'MLS on MBS',
+            imgWidth: '90%',
+            imgMaxWidth: undefined,
+            imgHeight: undefined,
+            imgMaxHeight: undefined,
+            imgTransform: undefined,
+            headerText: 'Advertise with us.',
+            descriptorText: "Broadcast your ads on MBS. For free.",
+            renderButton: true,
+            buttonText: 'Get Started',
+            link: '/', 
+        },
+    }
 
     return (
         <>
@@ -18,6 +78,9 @@ function MBS() {
             </Helmet>
             <div className={styles.container}>
                 <LiveMBS />
+                <div className={styles.topPost}>
+                    <Carousel propsObj={carouselProps} />
+                </div>
             </div>
         </>
     )
