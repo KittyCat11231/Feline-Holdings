607724aa45e2132fa5f643b6cc887375dfb4ad8a
KittyCat11231 <63092830+KittyCat11231@users.noreply.github.com>
2024-07-10 20:24:40 -0400

    update src/intraroute/components/SearchBarTest.js

--- src/intraroute/components/SearchBarTest.js
+++ src/intraroute/components/SearchBarTest.js
@@ -1,20 +1,43 @@
 import React from 'react';
 import { useState } from 'react';
+import styles from './SearchBarTest.module.css';
 
 import allStopsForSearch from '../scripts/search-data';
+
+function Dropdown(props) {
+  return (
+    <div className={styles.dropdownContainer}>
+      {props.filteredItems.map(item => {
+        return <p className={styles.searchItem}></div>
+      })}
+    </div>
+  )
+}
 
 function SearchBarTest() {
     console.log(allStopsForSearch[0]);
     const [items, setItems] = useState(allStopsForSearch);
     const [query, setQuery] = useState('');
+    
+    const [showDropdown, setShowDropdown] = useState(false);
 
     const filteredItems = items.filter(item => {
         return item.keywords.toLowerCase().includes(query.toLowerCase())
     })
+    
+    function handleChange(e) {
+      setQuery(e.target.value);
+      if (query.length > 0 && showDropdown === false) {
+        setShowDropdown(true);
+      } else if (query.length < 1 && showDropdown === true) {
+        setShowDropdown(false);
+      }
+    }
 
     return(
         <div>
-            <input value={query} type='search' onChange={e => setQuery(e.target.value)} />
+            <input value={query} type='search' onChange={handleChange(e
+            {showDropdown ? <Dropdown filteredItems={filteredItems} /> : ''}
             <h3>Results:</h3>
             {filteredItems.map(item => (
                 <div>{item.title}</div>
