7bfd1c6d090d9da969e38f61dc3eef62f78fa869
KittyCat11231 <kittycat61500@gmail.com>
2024-07-14 22:51:31 -0400

    testing

--- src/app/intra/page.js
+++ src/app/intra/page.js
@@ -1,3 +1,5 @@
+'use client'
+ 
 import React from 'react';
 import Intra from '../../pages/Intra';
 
--- src/app/intraroute/changelog/page.js
+++ src/app/intraroute/changelog/page.js
@@ -1,3 +1,5 @@
+'use client'
+ 
 import React from 'react';
 import IntraRouteChangelog from '../../../intraroute/pages/IntraRouteChangelog';
 
