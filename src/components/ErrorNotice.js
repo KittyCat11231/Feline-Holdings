810609ce5091ecd308914faa6741bba7a72fca7a
KittyCat11231 <kittycat61500@gmail.com>
2024-08-20 16:45:30 -0400

    i can spell

--- src/components/ErrorNotice.js
+++ src/components/ErrorNotice.js
@@ -6,7 +6,7 @@ function ErrorNotice() {
         <div className={styles.container}>
             <h1 className={`${styles.text} ${styles.text1}`}>8/20/2024</h1>
             <h1 className={`${styles.text} ${styles.text2}`}>IntraRoute is currently experiencing errors following an update to our route data.</h1>
-            <h1 className={`${styles.text} ${styles.text3}`}>We appreciate your parience while we work to resolve the issue.</h1>
+            <h1 className={`${styles.text} ${styles.text3}`}>We appreciate your patience while we work to resolve the issue.</h1>
         </div>
     )
 }
