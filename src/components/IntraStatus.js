39ef987906ee8303aa8dc32e3d94089ee3476b0e
KittyCat11231 <kittycat61500@gmail.com>
2024-10-03 14:03:16 -0400

    Update IntraStatus.js

--- src/components/IntraStatus.js
+++ src/components/IntraStatus.js
@@ -7,6 +7,7 @@ import intraBusLogo from '../intraroute/logos/intrabus-white.svg';
 import intraSailLogo from '../intraroute/logos/intrasail-white.svg';
 import intraBahnLogo from '../intraroute/logos/intrabahn-white.svg';
 
+import intraRail51 from '../intraroute/assets/intrarail/bullets/51-white.png';
 import intraRail54 from '../intraroute/assets/intrarail/bullets/54.png';
 
 function StatusLine(props) {
@@ -96,102 +97,22 @@ function IntraStatus() {
         <div className={styles.container}>
             <div className={styles.line}>
                 <StatusLine
-                    logoSrc={intraAirLogo}
-                    logoAlt='IntraAir'
+                    logoSrc={intraRailLogo}
+                    logoAlt='IntraRail'
                     modeColor='#4d4d4d'
-                    routeColor='#e6e6e6'
-                    routeName='Flight 145'
-                    routeNameColor='black'
-                    showRouteSymbol={false}
-                    routeSymbol={undefined}
-                    status='Suspended'
-                    statusColor='red'
+                    routeColor='red'
+                    routeName='Bee Twelver'
+                    routeNameColor='white'
+                    showRouteSymbol={true}
+                    routeSymbol={intraRail51}
+                    status='Station Closure'
+                    statusColor='olive'
                     statusTextColor='white'
-                    description='Service is suspended due to airport reconstruction at Geneva Bay New Indigo International Airport (GBE).'
-                    descriptionBoxColor='#f98585'
+                    description='Trains bypass Veldberg SE7.'
+                    descriptionBoxColor='#bab863'
                     descriptionColor='white'
-                    showLink={false}
-                    link='/'
-                />
-            </div>
-            <div className={styles.line}>
-                <StatusLine
-                    logoSrc={intraAirLogo}
-                    logoAlt='IntraAir'
-                    modeColor='#4d4d4d'
-                    routeColor='#e6e6e6'
-                    routeName='Flight 167'
-                    routeNameColor='black'
-                    showRouteSymbol={false}
-                    routeSymbol={undefined}
-                    status='Suspended'
-                    statusColor='red'
-                    statusTextColor='white'
-                    description='Service is suspended due to airport reconstruction at Caprica International Airport (CAP).'
-                    descriptionBoxColor='#f98585'
-                    descriptionColor='white'
-                    showLink={false}
-                    link='/'
-                />
-            </div>
-            <div className={styles.line}>
-                <StatusLine
-                    logoSrc={intraAirLogo}
-                    logoAlt='IntraAir'
-                    modeColor='#4d4d4d'
-                    routeColor='#e6e6e6'
-                    routeName='Flight 246'
-                    routeNameColor='black'
-                    showRouteSymbol={false}
-                    routeSymbol={undefined}
-                    status='Suspended'
-                    statusColor='red'
-                    statusTextColor='white'
-                    description='Service is suspended due to airport reconstruction at Geneva Bay New Indigo International Airport (GBE).'
-                    descriptionBoxColor='#f98585'
-                    descriptionColor='white'
-                    showLink={false}
-                    link='/'
-                />
-            </div>
-            <div className={styles.line}>
-                <StatusLine
-                    logoSrc={intraAirLogo}
-                    logoAlt='IntraAir'
-                    modeColor='#4d4d4d'
-                    routeColor='#e6e6e6'
-                    routeName='Flight 556'
-                    routeNameColor='black'
-                    showRouteSymbol={false}
-                    routeSymbol={undefined}
-                    status='Suspended'
-                    statusColor='red'
-                    statusTextColor='white'
-                    description='Service is suspended due to airport reconstruction at Geneva Bay New Indigo International Airport (GBE).'
-                    descriptionBoxColor='#f98585'
-                    descriptionColor='white'
-                    showLink={false}
-                    link='/'
-                />
-            </div>
-            <div className={styles.line}>
-                <StatusLine
-                    logoSrc={intraAirLogo}
-                    logoAlt='IntraAir'
-                    modeColor='#4d4d4d'
-                    routeColor='#e6e6e6'
-                    routeName='Flight 652'
-                    routeNameColor='black'
-                    showRouteSymbol={false}
-                    routeSymbol={undefined}
-                    status='Suspended'
-                    statusColor='red'
-                    statusTextColor='white'
-                    description='Service is suspended due to airport reconstruction at Caprica International Airport (CAP).'
-                    descriptionBoxColor='#f98585'
-                    descriptionColor='white'
-                    showLink={false}
-                    link='/'
+                    showLink={true}
+                    link='https://wiki.minecartrapidtransit.net/index.php/IntraRail#Service_Changes'
                 />
             </div>
             <div className={styles.line}>
@@ -207,51 +128,11 @@ function IntraStatus() {
                     status='Station Closure'
                     statusColor='olive'
                     statusTextColor='white'
-                    description='Trains bypass Geneva Bay New Indigo International Airport due to airport reconstruction and accidental damage to our station.'
+                    description='Trains bypass Geneva Bay New Indigo International Airport.'
                     descriptionBoxColor='#bab863'
                     descriptionColor='white'
                     showLink={true}
                     link='https://wiki.minecartrapidtransit.net/index.php/IntraRail#Service_Changes'
-                />
-            </div>
-            <div className={styles.line}>
-                <StatusLine
-                    logoSrc={intraBusLogo}
-                    logoAlt='IntraBus'
-                    modeColor='#4d4d4d'
-                    routeColor='#e6e6e6'
-                    routeName='Route 102'
-                    routeNameColor='black'
-                    showRouteSymbol={false}
-                    routeSymbol={undefined}
-                    status='Suspended'
-                    statusColor='red'
-                    statusTextColor='white'
-                    description='Service is suspended due to road construction near UCWT International Airport.'
-                    descriptionBoxColor='#f98585'
-                    descriptionColor='white'
-                    showLink={true}
-                    link='https://wiki.minecartrapidtransit.net/index.php/IntraBus#Service_Alert'
-                />
-            </div>
-            <div className={styles.line}>
-                <StatusLine
-                    logoSrc={intraBusLogo}
-                    logoAlt='IntraBus'
-                    modeColor='#4d4d4d'
-                    routeColor='#e6e6e6'
-                    routeName='Route 103'
-                    routeNameColor='black'
-                    showRouteSymbol={false}
-                    routeSymbol={undefined}
-                    status='Suspended'
-                    statusColor='red'
-                    statusTextColor='white'
-                    description='Service is suspended due to road construction near UCWT International Airport.'
-                    descriptionBoxColor='#f98585'
-                    descriptionColor='white'
-                    showLink={true}
-                    link='https://wiki.minecartrapidtransit.net/index.php/IntraBus#Service_Alert'
                 />
             </div>
         </div>
