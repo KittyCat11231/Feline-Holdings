ce0eac7004063af81b9d9b676df7129936254c05
KittyCat11231 <63092830+KittyCat11231@users.noreply.github.com>
2024-10-19 09:47:55 -0400

    update src/intraroute/components/ErrorNotice.js

--- src/intraroute/components/ErrorNotice.js
+++ src/intraroute/components/ErrorNotice.js
@@ -2,11 +2,23 @@ import React from 'react';
 import styles from './ErrorNotice.module.css';
 
 function ErrorNotice(props) {
+  let text1;
+  let text2;
+  if (props.text1 === 'default') {
+    text1 = 'IntraRoute is currently experiencing errors following an update to our route data.'
+  } else {
+    text1 = props.text1;
+  }
+  if (props.text2 = 'default') {
+    text2 = 'We appreciate your patience while we work to resolve the issue.'
+  } else {
+    text2 = props.text2;
+  }
     return (
         <div className={styles.container}>
             <h1 className={`${styles.text} ${styles.text1}`}>{props.date}</h1>
-            <h1 className={`${styles.text} ${styles.text2}`}>IntraRoute is currently experiencing errors following an update to our route data.</h1>
-            <h1 className={`${styles.text} ${styles.text3}`}>We appreciate your patience while we work to resolve the issue.</h1>
+            <h1 className={`${styles.text} ${styles.text2}`}>{props.text1}</h1>
+            <h1 className={`${styles.text} ${styles.text3}`}>{props.text2}</h1>
         </div>
     )
 }
